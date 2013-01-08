dba1c82a7b56c36fb7d52a7891c22376610a88cb
e-dwarf <edward@gronroos.se>
2013-01-08 18:28:19 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConferenceApp::Application.config.secret_token = '64155f097ca2b25adedc9372769b07f69a3013df2bfff4ca8b8ca6b706de8ba86d2fb029053e1890009d7888738f0587c61810df08d8848295e81cbe9a60cdf4'
