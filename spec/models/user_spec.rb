616901a7c606bcc769ccbbb2c85bfc44a76ff232
Ivar Østhus <ivarconr@gmail.com>
2013-01-17 22:41:11 +0100

    Added more spec for User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,24 @@ describe User do
   describe ".new" do
     it "reuires users to have email" do
       user = User.new(:email => nil)
-      expect user.invalid?
-      expect user.errors[:email]
+      user.invalid?.should be_true
+      user.errors[:email].should be_true
       expect(user.errors[:email].join).to eq('can\'t be blank')
+    end
+    it "reuires users to have name" do
+      user = User.new(:name => nil)
+      user.invalid?.should be_true
+      user.errors[:name].should be_true
+      expect(user.errors[:name].join).to eq('can\'t be blank')
+    end
+    it "should be valid with required fields" do
+      user = User.new({
+        :name => "Tester",
+        :email => "me@mail.com",
+        :tlf => "92043382",
+        :password => "lala12345",
+        :accepcted_privacy => true})
+      user.valid?.should be_true
     end
   end
 end
