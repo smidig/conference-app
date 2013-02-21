ce5f6b99e064ab26523bf45445d4b38d21fe81bc
Ivar Østhus <ivarconr@gmail.com>
2013-02-21 20:22:57 +0100

    added specs at price implementation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ActiveRecord::Base
   attr_accessible :comment, :completed, :owner_user_id
   has_many :users
   belongs_to :user, class_name: "User", foreign_key: "owner_user_id"
+
+  def price
+    sum = 0
+    self.users.each do |u|
+      sum += u.ticket.price
+    end
+    sum
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,21 +1,40 @@
 require 'spec_helper'
 
 describe Order do
-  describe "It should be possible to create order" do
-    #user = FactoryGirl.create(:user)
-    #user.save!
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @user2 = FactoryGirl.create(:user)
+    @user.ticket = Ticket.new({:name => "name", :price => 200})
+    @user2.ticket = Ticket.new({:name => "name", :price => 300})
+  end
+  it "should be possible to create order" do
 
-    #order = Order.new({
-    #    :completed => false,
-    #    :owner_user_id => user.id
-    #  })
+    order = Order.new({
+        :completed => false,
+        :owner_user_id => @user.id
+      })
 
-    #order.save!
-    #user.order_id = order.id
-    #user.save!
+    order.save!
+    @user.order_id = order.id
+    @user.save!
 
-    #order.users.size.should == 1
-    #order.users.first.id.should == user.id
-    #order.owner_user_id.should ==user.id
+    order.users.size.should == 1
+    order.users.first.id.should == @user.id
+    order.owner_user_id.should == @user.id
+  end
+  it "should sum prices based on users tickets" do
+
+    order = Order.new({
+        :completed => false,
+        :owner_user_id => @user.id
+      })
+
+    order.save!
+    @user.order_id = order.id
+    @user2.order_id = order.id
+    @user.save!
+    @user2.save!
+
+    order.price.should == 500
   end
 end
