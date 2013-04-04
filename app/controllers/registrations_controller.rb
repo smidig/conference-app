0843be6a78ee835ac1f34bbfb332dd436a8b40a6
Ivar Conradi Østhus <ivarconr@gmail.com>
2013-04-04 19:09:39 +0200

    Should set up variables for post also if it fails

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
   def new
-    @tickets = Ticket.visible
-    @ticket_default = Ticket.default
-
-    # Enable override of special tickets from param
-    special_ticket = Ticket.find_by_name(params[:ticket_name])
-    if special_ticket
-      @tickets.push special_ticket
-      @ticket_default = special_ticket
-    end
-
+    set_up_default_tickets
     super
   end
 
   def create
-  	puts "Overridden create method in devise"
+  	set_up_default_tickets
   	super
   end
 
@@ -25,6 +16,20 @@ class RegistrationsController < Devise::RegistrationsController
       # TODO: send user to my-profile if sponsor or organizer
       # TODO: Send user to add talk if speaker
       edit_user_registration_path
+    end
+  end
+
+  private
+
+  def set_up_default_tickets
+    @tickets = Ticket.visible
+    @ticket_default = Ticket.default
+
+    # Enable override of special tickets from param
+    special_ticket = Ticket.find_by_name(params[:ticket_name])
+    if special_ticket
+      @tickets.push special_ticket
+      @ticket_default = special_ticket
     end
   end
 
