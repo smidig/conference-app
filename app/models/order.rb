d03e00ab787a850ae0a3a89289637666c29b5ada
Jonas Amundsen <jonasba@gmail.com>
2013-04-06 16:44:51 +0200

    Niftier summation of total order price.
    By utilizing built in methods in Ruby and the Rails framework itself,
    one can sum the price of an order in a way that is in my opinion
    sweeter, neater and more mind-boggling. Also, it reduces the line count
    by a total of 6!

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,13 +6,7 @@ class Order < ActiveRecord::Base
   belongs_to :user, class_name: "User", foreign_key: "owner_user_id"
 
   def price
-    totalSum = 0
-    self.users.each do |u|
-      unless u.ticket.nil?
-        totalSum += u.ticket.price
-      end
-    end
-    totalSum
+    users.map { |u| u.ticket and u.ticket.price }.compact.sum
   end
 
   def status
