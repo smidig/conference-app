682dfd727f36d6a79e77720d75dc84b1eabfea95
Ivar Conradi Østhus <ivarconr@gmail.com>
2013-04-10 20:44:15 +0200

    Factory creating user should use more random emails.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do |u|
     u.name 'Test User'
-    u.sequence(:email) { |n| "test#{n}@awesome.com"}
+    u.sequence(:email) { |n| "test#{n}"+rand(1000).to_s+"@awesome.com"}
     u.password 'please123445'
     u.tlf '99900999'
     u.accepted_privacy "1"
@@ -10,7 +10,7 @@ FactoryGirl.define do
 
   factory :admin, class: User do |u|
     u.name 'Test Admin User'
-    u.sequence(:email) { |n| "test#{n}@awesome.com"}
+    u.sequence(:email) { |n| "test#{n}"+rand(1000).to_s+"@awesome.com"}
     u.password 'please123445'
     u.tlf '99900999'
     u.accepted_privacy "1"
