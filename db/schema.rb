fb87cd094620407ee228d61396b87d7be3279fa6
e-dwarf <edward@gronroos.se>
2013-05-28 20:16:22 +0200

    Fixed create error in talks_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130429001338) do
+ActiveRecord::Schema.define(:version => 20130528175157) do
 
   create_table "cospeakers_talks", :id => false, :force => true do |t|
     t.integer "talk_id"
