0883db10cfec4e6d02eb12ac6af24af471552e49
Jonas Amundsen <jonasba@gmail.com>
2013-06-08 18:23:15 +0200

    Utilizing inheritance to create an admin factory.
    Read more about it in the documentation [1].
    
    [1]
    https://github.com/thoughtbot/factory_girl/blob/master/GETTING_STARTED.md#inheritance

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,14 +9,7 @@ FactoryGirl.define do
     u.ticket { FactoryGirl.create(:ticket) }
   end
 
-  factory :admin, class: User do |u|
-    u.name 'Test Admin User'
-    u.sequence(:email) { |n| "test#{n}"+rand(1000).to_s+"@awesome.com"}
-    u.password 'please123445'
-    u.tlf '99900999'
-    u.accepted_privacy "1"
-    u.company 'Smidig 2013'
-    u.ticket { FactoryGirl.create(:ticket) }
+  factory :admin, :class => User, parent: :user do |u|
     u.admin true
   end
 end
