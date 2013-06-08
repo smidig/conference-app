e40fbf3d5ca5188aefb6874ac332da21a8ea2773
Jonas Amundsen <jonasba@gmail.com>
2013-06-08 12:24:11 +0200

    Cleaning up code.
    Changes are largely inspired by the Ruby Style Guide [1] and includes
    the following.
    
    * Removing unnecessary return statements
    * Removing parentheses around conditionals
    * Removing control flow statements with negative conditions
    
    The following example show a bad control flow.
    
      if not anything
        do_something
      else
        do_something_else
      end
    
    I would rewrite it like this.
    
      if anything
        do_something_else
      else
        do_something
      end
    
    * Utilizing Rails' #try [2]
    
    This enables us to remove statements like
    
      current_user and corrent_user.admin
    
    and replace them with the simple statement
    
      current_user.try(:admin)
    
    * Replacing `or` and `and` with `||` and `&&`, respectively, where
      appropriate
    
    `or` and `and` are control flow operators and does not behave the same
    way as their seemingly similar alternatives [3].
    
    * Removing ternary operators returning only boolean values
    
      current_user.includes_dinner = params[:dinner] == "yes" ? true : false
    
    ..is the same as
    
      current_user.includes_dinner = params[:dinner] == "yes"
    
    * Fixing spelling errors
    
    Examples include 'uncomplete', 'adress'.
    
    * Removing unnecessary references to `self`
    * Removing unnecessary .gitkeep files
    
    [1] https://github.com/bbatsov/ruby-style-guide
    [2] http://api.rubyonrails.org/classes/Object.html#method-i-try
    [3] http://devblog.avdi.org/2010/08/02/using-and-and-or-in-ruby/

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,8 +1,6 @@
 class Sponsor < ActiveRecord::Base
   attr_accessible :description, :imageUrl, :name, :url
 
-
-  # validations
   validates_presence_of :name
 
   add_default_scheme :url, :imageUrl
