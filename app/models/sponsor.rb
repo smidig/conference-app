0ca54c1a7df7fbf0a5e20427232108f3f6be5438
Jonas Amundsen <jonasba@gmail.com>
2013-06-08 12:04:02 +0200

    Removing old before_filter.
    This was supposed to be removed with @dbd88e, but I completely forgot
    it.

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -5,7 +5,5 @@ class Sponsor < ActiveRecord::Base
   # validations
   validates_presence_of :name
 
-  before_save :prefix_url_with_default_scheme, :if => :url_changed?
-
   add_default_scheme :url, :imageUrl
 end
