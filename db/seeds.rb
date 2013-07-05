smidig/conference-app

commit cc2a16edb138ad6da75ed6a0d9e8cd14bc9be0b8

Lagt til gravatar på profilsiden og listen med brukere

db/seeds.rb
@@ -13,6 +13,7 @@ administrators = [
     {:name => "Jonas Amundsen",      :email => "jonasba@gmail.com"},
     {:name => "Ivar Conradi Østhus", :email => "ivarconr@gmail.com"},
     {:name => "Edward Grönroos", :email => "edward@gronroos.se"}
+    {:name => "Trude Martinsen", :email => "trude.martinsen@gmail.com"}
 ]
 
 administrators.each do |user_data|

app/assets/stylesheets/a_smidig_theme.scss
@@ -353,6 +353,14 @@ a:hover {
     }
   }
 
+  .profile{
+    #gravatar{
+      float:right;
+      padding:3px;
+      border: 1px solid white;
+    }
+  }
+  
   // Global styling of text-views...
   .content-wrapper {
     padding: 20px 20px 40px;

app/views/my_profile/index.html.haml
@@ -1,65 +1,68 @@
-%h2 Hei #{@user.name}
-%p
-  Velkommen til Smidig 2013. Dette er din personlige profil registrert hos oss.
+.profile
+  %h2 Hei #{@user.name}
+  %p
+    Velkommen til Smidig 2013. Dette er din personlige profil registrert hos oss.
 
-.row-fluid
-  .span6
-    .boxed
-      .title
-        %i.icon-user
-        Din profil
-      .content
-        :markdown
-          - **Navn**: #{@user.name}
-          - **Epost**: #{@user.email}
-          - **Telefon**: #{@user.tlf}
-          - **Twitter**: #{@user.twitter}
-          - **Firma**: #{@user.company}
-          - **Deltar på konferansemiddag**: <span class="dinner">#{@user.includes_dinner ? "Ja": "Nei"}</span>
-          - **Allergier**: #{@user.allergies.blank? ? "Ingen" : @user.allergies}
-          - **Billett**: #{@user.ticket.display} ([Kvittering](/my_profile/receipt))
-        = link_to (edit_user_registration_path) do
-          %i.icon-pencil
-          Endre din profil
-
-    - if @order
+  .row-fluid
+    .span6
       .boxed
         .title
-          %i.icon-shopping-cart
-          Din bestilling
+          %i.icon-user
+          Din profil
         .content
+          %img{:src => "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(@user.email)}?s=80&&d=mm", :id => "gravatar"}
           :markdown
-            - **Status**: #{@order.status}
-            - **Opprettet**: #{@order.created_at.to_formatted_s(:long)}
-            - **Eies av**: #{@order.owner.name}
-            - **Totalt pris**: kr #{@order.price},-
+            - **Navn**: #{@user.name}
+            - **Epost**: #{@user.email}
+            - **Telefon**: #{@user.tlf}
+            - **Twitter**: #{@user.twitter}
+            - **Firma**: #{@user.company}
+            - **Deltar på konferansemiddag**: <span class="dinner">#{@user.includes_dinner ? "Ja": "Nei"}</span>
+            - **Allergier**: #{@user.allergies.blank? ? "Ingen" : @user.allergies}
+            - **Billett**: #{@user.ticket.display} ([Kvittering](/my_profile/receipt))
+          = link_to (edit_user_registration_path) do
+            %i.icon-pencil
+            Endre din profil
 
-          - if @order.owner_user_id == @user.id
-            = link_to({:controller => "orders", :action => "show", :id => @order.id}) do
-              %i.icon-th-list
-              #{@order.completed ? "Se" : "Fullfør"} din bestilling
 
-  .span3
-    .boxed.dinner-question
-      .title
-        %i.icon-question-sign
-        Skal du på konferansemiddagen?
-      .content
-        = link_to "Ja", {:action => "dinner_registration", :dinner => "yes"}, :class => 'btn btn-primary', :remote => true
-        = link_to "Nei", {:action => "dinner_registration", :dinner => "no"}, :remote => true
+      - if @order
+        .boxed
+          .title
+            %i.icon-shopping-cart
+            Din bestilling
+          .content
+            :markdown
+              - **Status**: #{@order.status}
+              - **Opprettet**: #{@order.created_at.to_formatted_s(:long)}
+              - **Eies av**: #{@order.owner.name}
+              - **Totalt pris**: kr #{@order.price},-
 
-    .boxed.speaker
-      .title
-        %i.icon-bullhorn
-        Dine taler
-      .content
-        %ul
-          - for talk in Talk.where(:user_id => @user.id)
-            %li
-              = link_to talk.title, edit_talk_path(talk)
-              = " [#{talk.status}]"
-          - for talk in @user.talks
-            %li
-              = talk.title
-              = " [#{talk.status}]"
-        = link_to "Opprett foredrag", new_talk_path, :class => 'btn btn-sucess'
+            - if @order.owner_user_id == @user.id
+              = link_to({:controller => "orders", :action => "show", :id => @order.id}) do
+                %i.icon-th-list
+                #{@order.completed ? "Se" : "Fullfør"} din bestilling
+
+    .span3
+      .boxed.dinner-question
+        .title
+          %i.icon-question-sign
+          Skal du på konferansemiddagen?
+        .content
+          = link_to "Ja", {:action => "dinner_registration", :dinner => "yes"}, :class => 'btn btn-primary', :remote => true
+          = link_to "Nei", {:action => "dinner_registration", :dinner => "no"}, :remote => true
+
+      .boxed.speaker
+        .title
+          %i.icon-bullhorn
+          Dine taler
+        .content
+          %ul
+            - for talk in Talk.where(:user_id => @user.id)
+              %li
+                = link_to talk.title, edit_talk_path(talk)
+                = " [#{talk.status}]"
+            - for talk in @user.talks
+              %li
+                = talk.title
+                = " [#{talk.status}]"
+          = link_to "Opprett foredrag", new_talk_path, :class => 'btn btn-sucess'

app/views/users/index.html.haml
@@ -7,6 +7,7 @@
     %tr
       %th ID
       %th Dato
+      %th 
       %th Navn
       %th Epost
       %th Firma
@@ -20,6 +21,8 @@
       %tr
         %td= link_to "#{user.id}", edit_user_path(user)
         %td= user.created_at.to_formatted_s(:short)
+        %td
+          %img{:src => "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=35&&d=wavatar"}
         %td
           - if user.admin
             %i{:class => "icon-user", :title => "Admin"}

commit 5ffc1ef6dbf38b59fdcfd90e07fb82c29a2336fd

Fixed error introduced by cc2a16edb138ad6da75ed6a0d9e8cd14bc9be0b8

db/seeds.rb
@@ -12,7 +12,7 @@ Ticket.create(name: 'Speaker', price: 0, active: true, visible: false, ticket_ty
 administrators = [
     {:name => "Jonas Amundsen",      :email => "jonasba@gmail.com"},
     {:name => "Ivar Conradi Østhus", :email => "ivarconr@gmail.com"},
-    {:name => "Edward Grönroos", :email => "edward@gronroos.se"}
+    {:name => "Edward Grönroos", :email => "edward@gronroos.se"},
     {:name => "Trude Martinsen", :email => "trude.martinsen@gmail.com"}
 ]
 