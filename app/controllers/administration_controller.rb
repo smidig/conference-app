0f62d02e6a2df1fc69b6243bf6a1923da9fff6e9
Trude <trude.martinsen@gmail.com>
2013-08-08 20:36:14 +0200

    Fikset identering

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -1,7 +1,6 @@
 class AdministrationController < ApplicationController
   authorize_admin!
 
-
   def statistics
     users = User.all
     @antall = users.count
@@ -10,45 +9,41 @@ class AdministrationController < ApplicationController
     @antallPrJobbrolle = {}
     @antallPrFirma = {}
 
-    users.each do |user|
-    	if user.completed
-    		@antallBetalende += 1
-    	end
+    users.each do |user|   
+      if user.completed
+        @antallBetalende += 1
+      end
 
-        if @antallPrBillett[user.ticket.name] == nil
-            @antallPrBillett[user.ticket.name] = 1
-        else
-            @antallPrBillett[user.ticket.name] = @antallPrBillett[user.ticket.name] + 1
+      if @antallPrBillett[user.ticket.name] == nil
+        @antallPrBillett[user.ticket.name] = 1
+      else
+        @antallPrBillett[user.ticket.name] = @antallPrBillett[user.ticket.name] + 1
+      end
+
+      if user.role == '' or user.role == nil
+        if(@antallPrJobbrolle['Ikke definert'] == nil)
+          @antallPrJobbrolle['Ikke definert'] = 1
+        else 
+          @antallPrJobbrolle['Ikke definert'] = @antallPrJobbrolle['Ikke definert'] + 1
         end
-
-        if user.role == '' or user.role == nil
-            if(@antallPrJobbrolle['Ikke definert'] == nil)
-                @antallPrJobbrolle['Ikke definert'] = 1
-            else 
-                @antallPrJobbrolle['Ikke definert'] = @antallPrJobbrolle['Ikke definert'] + 1
-            end
+      else
+        if @antallPrJobbrolle[user.role] == nil
+          @antallPrJobbrolle[user.role] = 1
         else
-            if @antallPrJobbrolle[user.role] == nil
-                @antallPrJobbrolle[user.role] = 1
-            else
-                @antallPrJobbrolle[user.role] = @antallPrJobbrolle[user.role] + 1
-            end
+          @antallPrJobbrolle[user.role] = @antallPrJobbrolle[user.role] + 1
         end
+      end
 
-    	
-        if @antallPrFirma[user.company] == nil
-            @antallPrFirma[user.company] = 1
-        else
-            @antallPrFirma[user.company] = @antallPrFirma[user.company] + 1
-        
-        end
-
+      if @antallPrFirma[user.company] == nil
+        @antallPrFirma[user.company] = 1
+      else
+        @antallPrFirma[user.company] = @antallPrFirma[user.company] + 1
+      end
     end
 
     respond_to do |format|
-      format.html # index.html.erb
-      format.json  { render :json => @antallPrJobbrolle }
-    end
-
+    format.html # index.html.erb
+    format.json  { render :json => @antallPrJobbrolle }
   end
+end
 end
