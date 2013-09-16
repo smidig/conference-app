ef95b9c95c717f1dddc3909f511007136afc4ef9
Ivar Østhus <ivarconr@gmail.com>
2013-09-16 19:51:35 +0200

    Added caching to sponsors

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -4,4 +4,15 @@ class Sponsor < ActiveRecord::Base
   validates_presence_of :name
 
   add_default_scheme :url, :imageUrl
+
+  after_save    :expire_sponsor_all_cache
+  after_destroy :expire_sponsor_all_cache
+
+  def self.all_cached
+    Rails.cache.fetch('Sponsor.all') { all }
+  end
+
+  def expire_sponsor_all_cache
+    Rails.cache.delete('Sponsor.all')
+  end
 end
