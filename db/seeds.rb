dd47bf1a32d0bae1b8a5256b8cccf1b4cb962637
Ivar Østhus <ivarconr@gmail.com>
2013-10-06 12:28:02 +0200

    Program: seed for generating template layout and a set of talks

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ administrators.each do |user_data|
   user.save!
 end
 
-TalkType.create(name: 'Lyntale', duration: 10, visible: true)
+lyntale = TalkType.create(name: 'Lyntale', duration: 10, visible: true)
 TalkType.create(name: 'Kort workshop', duration: 45, visible: true)
 TalkType.create(name: 'Lang workshop', duration: 90, visible: true)
 
@@ -41,15 +41,43 @@ TalkCategory.create(name: 'Lean startup')
 
 Sponsor.create(name: "Smidig 2013", url: "http://2013.smidig.no/info/sponsor", imageUrl: "https://googledrive.com/host/0Bxbse3ziIO6GUmYwQ0VxME1TRUE/sponsor.png")
 
-Room.create(name: "Olympia")
-Room.create(name: "Film")
-Room.create(name: "Kunst")
-Room.create(name: "Foajé")
-Room.create(name: "Madonna")
-Room.create(name: "Vampyr")
-Room.create(name: "Resturant")
+# Approved talks:
+for i in 0..10
+  t = Talk.create(
+    title: "Lorem ipsum test " + i.to_s, 
+    description: "Lorem ipusm...", 
+    talk_type_id: lyntale.id,
+    talk_category_id: TalkCategory.all.first.id)
+  t.status = "approved_and_confirmed";
+  t.user = User.all.first
+  t.save!
+end
 
-Timeslot.create(start: "2013-11-05 08:00:00", end: "2013-11-05 08:45:00")
-Timeslot.create(start: "2013-11-05 08:45:00", end: "2013-11-05 09:00:00")
-Timeslot.create(start: "2013-11-05 09:00:00", end: "2013-11-05 10:00:00")
-Timeslot.create(start: "2013-11-05 10:15:00", end: "2013-11-05 11:00:00")
+# rooms
+foaje = Room.create(name: "Foajé")
+olympia = Room.create(name: "Olympia")
+film = Room.create(name: "Film")
+kunst = Room.create(name: "Kunst")
+madonna = Room.create(name: "Madonna")
+vampyr = Room.create(name: "Vampyr")
+resturant = Room.create(name: "Resturant")
+
+
+t1 = Timeslot.create(start: "2013-11-05 08:00:00", end: "2013-11-05 08:45:00")
+t2 = Timeslot.create(start: "2013-11-05 08:45:00", end: "2013-11-05 09:00:00")
+t3 = Timeslot.create(start: "2013-11-05 09:00:00", end: "2013-11-05 10:00:00")
+t4 = Timeslot.create(start: "2013-11-05 10:15:00", end: "2013-11-05 11:00:00")
+
+Roomslot.create(:room_id => foaje.id, :timeslot_id => t1.id)
+
+Roomslot.create(:room_id => olympia.id, :timeslot_id => t2.id)
+Roomslot.create(:room_id => olympia.id, :timeslot_id => t2.id)
+
+# lyntaler row 1
+Roomslot.create(:room_id => olympia.id, :timeslot_id => t3.id)
+Roomslot.create(:room_id => film.id, :timeslot_id => t3.id)
+Roomslot.create(:room_id => kunst.id, :timeslot_id => t3.id)
+Roomslot.create(:room_id => madonna.id, :timeslot_id => t3.id)
+Roomslot.create(:room_id => vampyr.id, :timeslot_id => t3.id)
+
+
