5498153cfa83c0689d0fb9a39bbc6fdc83bbdc0f
Edward Grönroos <edward@gronroos.se>
2013-10-15 20:14:44 +0200

    updated program view

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -3,13 +3,51 @@ class ProgramController < ApplicationController
   layout "fullwidth"
 
   def index
-    #@roomslots = Roomslot.all
-
-    # @timeslot = Timeslot.find(:all, :group => :start)
+    @program = get_program_hash
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @talk_types }
+    end
+  end
+
+  def get_program_hash
+    program = {:max_roomslots => 0, :start_wrappers => []}
+
+    prev_start = Date.new(1970,01,01)
+    start_wrapper = nil
+    room_count = 0
+    Timeslot.all.each do |t|
+      puts t.roomslot.count
+
+      if t.start > prev_start
+        prev_start = t.start
+        start_wrapper[:room_count] = room_count if start_wrapper
+        start_wrapper = {:timeslots => [], :start => t.start, :room_count => 0}
+        program[:start_wrappers].push(start_wrapper)
+
+        if program[:max_roomslots] < room_count
+          program[:max_roomslots] = room_count
+        end
+        room_count = 0
+      end
+      
+      start_wrapper[:timeslots].push(t)
+      room_count += t.roomslot.size
+    end
+
+    start_wrapper[:room_count] = room_count
+    if program[:max_roomslots] < room_count
+      program[:max_roomslots] = room_count
+    end
+
+    program
+  end
+
+  def program
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: get_program_hash }
     end
   end
 
