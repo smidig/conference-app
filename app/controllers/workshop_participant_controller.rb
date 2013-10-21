271c2bb5820bdcc46c544bcd3e9f5d1fae3b7f2b
Ivar Østhus <ivarconr@gmail.com>
2013-10-21 20:48:38 +0200

    inital model used for workshop participants

---
+++ app/controllers/workshop_participant_controller.rb
@@ -0,0 +1,22 @@
+# -*- encoding : utf-8 -*-
+class WorkshopParticipantController < ApplicationController
+  authorize_user! :only => [:create, :destroy]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
