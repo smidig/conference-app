be088f604ad8866b37d1ba3cfb3aacc84a9f201f
Edward Grönroos <edward@gronroos.se>
2013-10-22 22:39:16 +0200

    updated details on program view

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -7,36 +7,44 @@ class ProgramController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @talk_types }
+      format.json { render json: @program }
     end
   end
 
+  # Should be rewritten to become a view model
   def get_program_hash
-    program = {:max_roomslots => 0, :start_wrappers => []}
+    program = {:max_roomslots => 0, :days => []}
 
     prev_start = Date.new(1970,01,01)
-    start_wrapper = nil
+    start = nil
+    day = nil
     room_count = 0
     Timeslot.all.each do |t|
-      puts t.roomslot.count
+
+      if(t.start.to_date > prev_start.to_date)
+        day = {:starts => [], :date => t.start.to_date}
+        program[:days].push(day)
+      end
 
       if t.start > prev_start
         prev_start = t.start
-        start_wrapper[:room_count] = room_count if start_wrapper
-        start_wrapper = {:timeslots => [], :start => t.start, :room_count => 0}
-        program[:start_wrappers].push(start_wrapper)
-
+        
+        start[:room_count] = room_count if start
+        start = {:timeslots => [], :start => t.start, :room_count => 0}
+        
+        day[:starts].push(start)
+        
         if program[:max_roomslots] < room_count
           program[:max_roomslots] = room_count
         end
         room_count = 0
       end
-      
-      start_wrapper[:timeslots].push(t)
+
+      start[:timeslots].push(t)
       room_count += t.roomslot.size
     end
 
-    start_wrapper[:room_count] = room_count
+    start[:room_count] = room_count
     if program[:max_roomslots] < room_count
       program[:max_roomslots] = room_count
     end
