e7a96ae98694e19dd75ce7c4e9b9688e486d05b1
Ivar Østhus <ivarconr@gmail.com>
2013-10-23 20:30:50 +0200

    Added view for listing ws-participants

--- app/controllers/workshop_participant_controller.rb
+++ app/controllers/workshop_participant_controller.rb
@@ -1,9 +1,20 @@
 # -*- encoding : utf-8 -*-
 class WorkshopParticipantController < ApplicationController
-  authorize_user! :only => [:create, :destroy]
+  authorize_user!
 
   def index
-
+    @talk = Talk.find(params[:talk_id])
+    
+    if @talk.user_ids.include?(current_user.id) || current_user.admin
+      @participants = @talk.workshop_participants.collect {|p| p.user }
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml  { render :xml => @participants }
+      end
+    else
+      flash[:error] = "Access Denied"
+      redirect_to new_user_path
+    end
   end
 
   def show
