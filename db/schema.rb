b9bb8deaef036aa06d25cdcb834cd73b6c989cd5
Ivar Østhus <ivarconr@gmail.com>
2013-10-24 17:25:56 +0200

    corrected merge of db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20131023153722) do
-=======
-ActiveRecord::Schema.define(:version => 20131021182247) do
->>>>>>> dbd7cf75e61c6de7f5269c3a5240abc1b2ae43dd
 
   create_table "orders", :force => true do |t|
     t.string   "comment"
