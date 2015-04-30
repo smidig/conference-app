9f2f19c3a0f6218af1fcbabf9e8a9d52beef327e
Chris Searle <chris@chrissearle.org>
2015-04-30 13:43:37 +0200

    Update dates in mailers, config and database

--- app/models/smidig_mailer.rb
+++ app/models/smidig_mailer.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 
 class SmidigMailer < ActionMailer::Base
-  default_url_options[:host] = "2014.smidig.no"
-  FROM_EMAIL = 'Smidig 2014 <kontakt@smidig.no>'
-  SUBJECT_PREFIX = "[Smidig 2014]"
+  default_url_options[:host] = '2015.smidig.no'
+  FROM_EMAIL = 'Smidig 2015 <kontakt@smidig.no>'
+  SUBJECT_PREFIX = '[Smidig 2015]'
 
   default :from => FROM_EMAIL
 
