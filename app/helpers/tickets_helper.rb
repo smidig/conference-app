840a99b36fb04790aa076f6f265489386057013b
Chris Searle <chris@chrissearle.org>
2015-04-30 15:46:27 +0200

    Fetch ticket price from database in intro

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -6,4 +6,8 @@ module TicketsHelper
   def ticket_price(ticket)
     number_to_currency(ticket.price, unit: "NOK", separator: ",", delimiter: ".", format: "%u %n").gsub(/,00/, ',-')
   end
+
+  def regular_ticket_price
+    ticket_price(Ticket.where( :name => 'Regular 2015').first)
+  end
 end
