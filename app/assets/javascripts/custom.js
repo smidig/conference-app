60cbf5ecd05ba96ee5eb9b086a2d30ba54b466e7
Chris Searle <chris@chrissearle.org>
2015-10-20 20:39:23 +0200

    Remove unused initialization of old tweet library

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,15 +1,2 @@
 jQuery(function($){
-   $(".tweet").tweet({
-      username: "smidig",
-      join_text: "auto",
-      avatar_size: 0,
-      count: 4,
-      auto_join_text_default: "we said,",
-      auto_join_text_ed: "we",
-      auto_join_text_ing: "we were",
-      auto_join_text_reply: "we replied to",
-      auto_join_text_url: "we were checking out",
-      loading_text: "loading tweets...",
-      template: "{text}"
-   });
 });
