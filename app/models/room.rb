dbec9d510da392017b0b917a483a6bfa8c3650dc
Chris Searle <chris@chrissearle.org>
2015-10-20 14:15:44 +0200

    More room admin page stuff

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,12 @@ class Room < ActiveRecord::Base
   attr_accessible :details, :name, :color, :priority, :size
   has_many :roomslot
   default_scope :order => 'priority DESC'
+
+  def talk_count
+    self.roomslot.includes(:talks).map(&:talks).map(&:count).sum
+  end
+
+  def can_delete?
+    self.talk_count == 0
+  end
 end
